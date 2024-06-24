fb22fce547fa1561b7f4976166d4762516980413
LuisYepes601 <yepesluis006@gmail.com>
2024-06-24 11:44:03 -0500

    Estoy seguro de este commit

---
+++ js/app.js
@@ -0,0 +1,60 @@
+/**var diaHoy =prompt("¿Que día de la semana es hoy?".toLowerCase)
+
+if (diaHoy === "sabado" ||  dia === "domingo"){
+alert("Buen fin de semana")
+}else{
+    alert("Buena semana")
+}*/
+
+/**Crea un contador que comience en 1 y vaya hasta 10 usando un bucle 'while'. Muestra cada número.
+ * let contador = 1;
+while(contador<=10){
+alert("Voy en el bucle " + contador)
+contador++;
+}
+*/
+
+/** Crea un contador que comience en 10 y vaya hasta 0 usando un bucle 'while'. Muestra cada número. */
+/**
+let contador = 10;
+while(contador >= 0){
+    alert("Voy en el bucle: " + contador)
+    contador--;
+}
+
+*/
+
+/**
+ * Crea un programa de cuenta progresiva. Pide un número y cuenta desde 0 
+ * hasta ese número utilizando un bucle 'while' en la consola del navegador.
+ */
+
+/**
+ * cont = 0;
+let num =  parseInt (prompt("Ingresa un número"));
+while(num >= cont){
+    alert("La cuenta para llegar a " + num + " es: " + cont);
+cont ++;
+}
+ */
+
+/**Crea un programa de cuenta progresiva. 
+ * Pide un número y cuenta desde 0 hasta ese número utilizando un bucle 'while' en la consola del navegador. */
+
+/**
+ * let cont = 0;
+ num = prompt("Ingresa un número");
+ alert("Cuenta regresiva para llegar a: " + num)
+ 
+ while(num >= cont){ 
+    alert(cont)
+    cont++;
+ }
+
+ */
+
+var perro = "guau";
+patas = 4;
+
+(perro == "guau" && patas == 4 ? alert("Es un perro") : alert("Es otro animal"))
+
